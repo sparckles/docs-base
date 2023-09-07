0ab5b021b70faa3c8aa5ea35e60151c0f9f19ece
Sanskar Jethi <sansyrox@gmail.com>
2023-09-07 02:01:41 +0200

    add testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,161 +1,190 @@
-const featuredTestimonial = {
-    body: 'Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.',
+let testimonials = [
+  {
+    body: "Robyn has revolutionized the way I develop web solutions. Its seamless integration of Python's async capabilities with a Rust runtime not only ensures reliability and scalability but also provides quick project setup, a delightful user experience, and robust plugin support. With its exceptional speed and multithreaded efficiency, Robyn's real-time communication through WebSockets and dynamic URL routing has empowered me to create highly performant and interactive applications while maintaining full control over navigation and workflows. A game-changer for modern web development!",
     author: {
-      name: 'Brenna Goyette',
-      handle: 'brennagoyette',
+      name: 'Kunal Kushwaha',
+      handle: 'kunalstwt',
       imageUrl:
-        'https://i...content-available-to-author-only...h.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80',
-      logoUrl: 'https://t...content-available-to-author-only...i.com/img/logos/savvycal-logo-gray-900.svg',
+        'https://pbs.twimg.com/profile_images/1661046102756323329/4fmGAMnN_400x400.jpg',
+      title: 'DevRel manager at Civo',
     },
-  }
-  const testimonials = [
-    [
-      [
-        {
-          body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
-          author: {
-            name: 'Leslie Alexander',
-            handle: 'lesliealexander',
-            imageUrl:
-              'https://i...content-available-to-author-only...h.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        // More testimonials...
-      ],
-      [
-        {
-          body: 'Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.',
-          author: {
-            name: 'Lindsay Walton',
-            handle: 'lindsaywalton',
-            imageUrl:
-              'https://i...content-available-to-author-only...h.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        // More testimonials...
-      ],
-    ],
-    [
-      [
-        {
-          body: 'Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.',
-          author: {
-            name: 'Tom Cook',
-            handle: 'tomcook',
-            imageUrl:
-              'https://i...content-available-to-author-only...h.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        // More testimonials...
-      ],
-      [
-        {
-          body: 'Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.',
-          author: {
-            name: 'Leonard Krasner',
-            handle: 'leonardkrasner',
-            imageUrl:
-              'https://i...content-available-to-author-only...h.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-          },
-        },
-        // More testimonials...
-      ],
-    ],
-  ]
-  
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
-  
-  export default function Testimonials() {
-    return (
-      <div className="relative isolate  pb-32 pt-24 sm:pt-32">
+  },
+
+  {
+    body: "Having worked with a company building a Rust based open source search engine for over a year, I strongly believe in the notion that rewriting software with Rust can significantly improve software performance. Sanskar's idea to recreate Flask with Rust, was just incredible. Having used Robyn myself, it is refreshing to see such a performant Python framework and just the amazing developer ecosystem around it. Yes it still new and being developed, but I can say this with confidence that given the underlying Rust-based multithreaded run time will provide immense performance for running high throughout applications. I am glad to be one of the early sponsors and adopters for Robyn!",
+    author: {
+      name: 'Shivay Lamba',
+      handle: 'howdevelop',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1589996761086500865/IBdsm4RZ_400x400.jpg',
+      title: 'Developer Experience Engineer at MeiliSearch',
+    },
+  },
+  {
+    body: "I'm impressed with Robyn. It's a fast asynchronous web framework for the Python ecosystem that's built on top of Rust. The syntax is similar to other popular web frameworks, so it's easy to learn and be productive with. I've been using it to build web applications and services, and I'm really happy with the results. I'm also impressed with the Robyn community. They are very supportive and the developers are very responsive to feedback",
+    author: {
+      name: 'Carlos A. Marcano Vargas',
+      handle: 'carlos_marcv',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1418988844347793410/SHKOyqB3_400x400.jpg',
+      title: 'Technical Writer',
+    },
+  },
+  // More testimonials...
+  {
+    body: 'Great to see a Community Driven Open Source project, achieve new heights! Robyn is built by the community for the community',
+    author: {
+      name: 'Eddie Jaoude',
+      handle: 'eddiejaoude',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1640177590601416704/_Gs3eest_400x400.jpg',
+      title: 'Creator of EddieHub',
+    },
+  },
+  // More testimonials...
+  {
+    body: 'I used to be a Batman fan, but having met Robyn I now think the sidekick has become the hero. Free, OSS, straight forward and powerful, what is not to love?',
+    author: {
+      name: 'GrahamTheDev',
+      handle: 'GrahamTheDev',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1674010320057032706/NKa0EtQ8_400x400.jpg',
+      title: 'The Accessibility First DevRel ',
+    },
+  },
+  // More testimonials...
+  {
+    body: 'Robyn has made a big difference in my projects. Its flexible structure allows my work to adapt smoothly to my needs, even when I face complex challenges. The community-driven and open-source nature of Robyn makes it a welcoming place for developers like me. Plus, its simple yet powerful API has greatly streamlined my development process, reducing my wor oad. I highly recommend it!',
+    author: {
+      name: 'Julia Furst Morgado',
+      handle: 'juliafmorgado',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1140975948096868352/HJN-bkyS_400x400.jpg',
+      title: 'Global technologist @Veeam',
+    },
+  },
+  // More testimonials...
+  {
+    body: 'Robyn opens a new chapter in the Python web frameworks scene: the Rust powered one, where performance and safety are not the sole protagonists.',
+    author: {
+      name: 'Giovanni Barillari',
+      handle: 'gi0baro',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/492996183439052800/zVEX94M__400x400.jpeg',
+      title: 'Author of Granian and Emmett',
+    },
+  },
+  // More testimonials...
+  {
+    body: "I collaborate with Robyn's team and I must say, Sanskar does an excellent job maintaining the community. The project as a whole is immensely beneficial, both for collaboration and its practical uses. The tool is impressive, easy to use and the entire community is very welcoming to first-time contributors",
+    author: {
+      name: 'Jyoti Bisht',
+      handle: 'joeyousss',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1595447618707984385/XF3PQqTu_400x400.jpg',
+      title: 'Community Support Engineer at Appwrite',
+    },
+  },
+  // More testimonials...
+  {
+    body: "Robyn is a breath of fresh air in web development. Merging Python's simplicity with Rust's speed, it offers a seamless experience for developers. Its features are precisely what today's web projects need. I'm particularly impressed with its community focus; it's evident that everyone's voice matters in shaping Robyn's journey. In a sea of web frameworks, Robyn stands out not just for its tech but also for its heart.",
+    author: {
+      name: 'Francesco Ciulla',
+      handle: 'FrancescoCiull4',
+      imageUrl:
+        'https://pbs.twimg.com/profile_images/1617044903636123650/pYUcGGOu_400x400.jpg',
+      title: 'DevRel at daily.dev',
+    },
+  },
+  // More testimonials...
+]
+
+//shuflle testimonials
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  )
+
+export default function Testimonials() {
+  const chunkedTestimonials = chunk(
+    testimonials.sort(() => 0.5 - Math.random()),
+    3
+  )
+
+  return (
+    <div className="relative isolate  pb-32 pt-24 sm:pt-32">
+      <div
+        className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
+        aria-hidden="true"
+      >
         <div
-          className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
-          aria-hidden="true"
-        >
-          <div
-            className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-yellow-400 flex-none origin-top-right rotate-[30deg] xl:mr-[calc(50%-12rem)]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
+          className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-yellow-400 xl:mr-[calc(50%-12rem)]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
+        aria-hidden="true"
+      >
         <div
-          className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
-          aria-hidden="true"
-        >
-          <div
-            className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg]  xl:ml-0 xl:mr-[calc(50%-12rem)]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg]  xl:ml-0 xl:mr-[calc(50%-12rem)]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-xl text-center">
+          <h2 className="text-lg font-semibold leading-8 tracking-tight text-yellow-400">
+            Testimonials
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Some amazing people have said nice things about us.
+          </p>
         </div>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-xl text-center">
-            <h2 className="text-lg font-semibold leading-8 tracking-tight text-yellow-400">Testimonials</h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Some amazing people have said nice things about us.
-            </p>
-          </div>
-          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
-            <figure className="col-span-2 hidden sm:block sm:rounded-2xl sm:bg-white/5 hover:bg-white/10 duration-200 sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
-              <blockquote className="p-12 text-xl font-semibold leading-8 tracking-tight text-white">
-                <p>{`“${featuredTestimonial.body}”`}</p>
-              </blockquote>
-              <figcaption className="flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
-                <img
-                  className="h-10 w-10 flex-none rounded-full bg-white/5"
-                  src={featuredTestimonial.author.imageUrl}
-                  alt=""
-                />
-                <div className="flex-auto">
-                  <div className="font-semibold text-white">{featuredTestimonial.author.name}</div>
-                  <div className="text-yellow-400">{`@${featuredTestimonial.author.handle}`}</div>
-                </div>
-                <img className="h-10 w-auto flex-none" src={featuredTestimonial.author.logoUrl} alt="" />
-              </figcaption>
-            </figure>
-            {testimonials.map((columnGroup, columnGroupIdx) => (
-              <div key={columnGroupIdx} className="space-y-8 xl:contents xl:space-y-0">
-                {columnGroup.map((column, columnIdx) => (
-                  <div
-                    key={columnIdx}
-                    className={classNames(
-                      (columnGroupIdx === 0 && columnIdx === 0) ||
-                        (columnGroupIdx === testimonials.length - 1 && columnIdx === columnGroup.length - 1)
-                        ? 'xl:row-span-2'
-                        : 'xl:row-start-1',
-                      'space-y-8'
-                    )}
-                  >
-                    {column.map((testimonial) => (
-                      <figure
-                        key={testimonial.author.handle}
-                        className="rounded-2xl bg-white/5 hover:bg-white/10 duration-200 p-6 shadow-lg ring-1 ring-gray-900/5"
-                      >
-                        <blockquote className="text-white">
-                          <p>{`“${testimonial.body}”`}</p>
-                        </blockquote>
-                        <figcaption className="mt-6 flex items-center gap-x-4">
-                          <img className="h-10 w-10 rounded-full bg-gray-50" src={testimonial.author.imageUrl} alt="" />
-                          <div>
-                            <div className="font-semibold text-white">{testimonial.author.name}</div>
-                            <div className="text-yellow-400">{`@${testimonial.author.handle}`}</div>
-                          </div>
-                        </figcaption>
-                      </figure>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-1 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-3">
+          {chunkedTestimonials.map((columnGroup, columnGroupIdx) => (
+            <div key={columnGroupIdx} className="space-y-8  ">
+              {columnGroup.map((testimonial) => (
+                <figure
+                  key={testimonial.author.handle}
+                  className="rounded-2xl bg-white/5 p-6 shadow-lg ring-1 ring-gray-900/5 duration-200 hover:bg-white/10"
+                >
+                  <blockquote className="text-white">
+                    <p>{`“${testimonial.body}”`}</p>
+                  </blockquote>
+                  <figcaption className="mt-6 flex items-center gap-x-4">
+                    <img
+                      className="h-10 w-10 rounded-full bg-gray-50"
+                      src={testimonial.author.imageUrl}
+                      alt=""
+                    />
+                    <div>
+                      <div className="font-semibold text-white">
+                        {testimonial.author.name}
+                      </div>
+                      <div className="text-yellow-400">{`@${testimonial.author.handle}`}</div>
+                      <div className="font-normal text-white">
+                        {testimonial.author.title}
+                      </div>
+                    </div>
+                  </figcaption>
+                </figure>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
+
